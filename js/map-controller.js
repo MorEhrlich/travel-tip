886d2041ca1d02beb26ad907850af754b25cf516
Mor Ehrlich <mor.ehrlich@gmail.com>
2020-12-16 14:21:01 +0200

    add api google maps

--- js/map-controller.js
+++ js/map-controller.js
@@ -66,7 +66,7 @@ function getUserPosition() {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyBTd-r9ES9me88-mTQasKgom191cNMihjY'; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
